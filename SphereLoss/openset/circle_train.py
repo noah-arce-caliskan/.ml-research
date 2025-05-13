85e794860ba751b94284be0c1ab252d4d87718ea
noah-arce-caliskan <nac@uconn.edu>
2025-05-13 15:42:20 -0400

    attempted circle loss, results paled in comparison

---
+++ SphereLoss/openset/circle_train.py
@@ -0,0 +1,130 @@
+import os, argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import losses_nets
+from losses_nets import CircleLoss, convert_label_to_similarity
+from sklearn.metrics import roc_curve, auc
+from gen_hist import histogram
+import torch.nn.functional as F
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--lr', type=float, default=.001)
+parser.add_argument('--bs', type=int, default=256)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--optimizer', type=str, default='adamw')
+parser.add_argument('--hist_path', type=str, default='/home/noah/SphereLoss/openset/histograms/hist.png')
+parser.add_argument('--train_path', type=str, default='/home/noah/SphereLoss/openset/train')
+parser.add_argument('--val_path', type=str, default="/home/noah/SphereLoss/openset/val")
+parser.add_argument('--test_path', type=str, default='/home/noah/SphereLoss/openset/test')
+parser.add_argument('--best_weights', type=str, default='/home/noah/SphereLoss/openset/weights/best_weights.pth')
+
+parser.add_argument('--m', type=float, default=0.25)
+parser.add_argument('--gamma', type=float, default=80)
+args = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_transform = transforms.Compose([
+    transforms.Resize((112,112)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop((112,112), padding=10),
+    transforms.ColorJitter(0.5,0.5,0.5),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5]),
+    transforms.RandomErasing(p=0.4, scale=(0.02,0.4), ratio=(0.3,3.3))
+])
+eval_transform = transforms.Compose([
+    transforms.Resize((112,112)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
+])
+
+train_ds = datasets.ImageFolder(args.train_path, transform=train_transform)
+val_ds = datasets.ImageFolder(args.val_path, transform=eval_transform)
+test_ds = datasets.ImageFolder(args.test_path, transform=eval_transform)
+train_loader = DataLoader(train_ds, batch_size=args.bs, shuffle=True, num_workers=6)
+val_loader = DataLoader(val_ds, batch_size=args.bs, shuffle=False, num_workers=6)
+test_loader = DataLoader(test_ds, batch_size=args.bs, shuffle=False, num_workers=6)
+
+net = losses_nets.ResNet50Embedder(feature_dim=256, dropout_p=0.2, pretrained=True).to(device)
+net.feature = True
+
+criterion = CircleLoss(m=args.m, gamma=args.gamma).to(device)
+
+if args.optimizer.lower() == 'sgd':
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=.0001)
+elif args.optimizer.lower() == 'adam':
+    optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=.0001)
+else:
+    optimizer = optim.AdamW(net.parameters(), lr=args.lr, weight_decay=.0001)
+
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.6, patience=3)
+
+def val_metrics(embeddings, labels):
+    sim = embeddings @ embeddings.T
+    eq = labels.unsqueeze(0) == labels.unsqueeze(1)
+    mask_pos = torch.triu(eq, 1)
+    mask_neg = torch.triu(~eq, 1)
+    same = sim[mask_pos].cpu().numpy()
+    diff = sim[mask_neg].cpu().numpy()
+
+    y_true = np.concatenate([np.ones_like(same), np.zeros_like(diff)])
+    y_score= np.concatenate([same, diff])
+    fpr, tpr, _ = roc_curve(y_true, y_score)
+    roc_auc = auc(fpr, tpr)
+    fnr = 1 - tpr
+    eer = fpr[np.nanargmin(np.abs(fnr - fpr))]
+    return roc_auc, eer
+
+best_eer, no_improve, patience = 1.0, 0, 10
+
+for epoch in range(args.epochs):
+    net.train()
+    total_loss = 0
+    for imgs, lbls in train_loader:
+        imgs, lbls = imgs.to(device), lbls.to(device)
+        optimizer.zero_grad()
+        
+        embeds = net(imgs)
+        embeds = F.normalize(embeds, p=2, dim=1)
+
+        sp, sn = convert_label_to_similarity(embeds, lbls)
+        loss = criterion(sp, sn)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+    avg_loss = total_loss / len(train_loader)
+
+    net.eval()
+    emb_val, lbl_val = [], []
+    with torch.no_grad():
+        for imgs, lbls in val_loader:
+            emb_val.append(net(imgs.to(device)).cpu())
+            lbl_val.append(lbls)
+    emb_val = torch.cat(emb_val)
+    lbl_val = torch.cat(lbl_val)
+    val_auc, val_eer = val_metrics(emb_val, lbl_val)
+    print(f"Epoch {epoch+1} Loss: {avg_loss:.3f} Val AUC: {val_auc:.3f} Val EER: {val_eer:.3f}")
+
+    scheduler.step(avg_loss)
+    if val_eer < best_eer:
+        best_eer = val_eer
+        torch.save(net.state_dict(), args.best_weights)
+        no_improve = 0
+    else:
+        no_improve += 1
+        if no_improve >= patience:
+            print('Not improving. Stopping.')
+            break
+
+net.load_state_dict(torch.load(args.best_weights))
+histogram(net, args.bs)
+print('Best weights and histogram saved.')
